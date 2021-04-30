328ccf3760dfba0e72bd2cc76893c4832c5fd52c
Jon Dayley <jonathandayley@gmail.com>
2021-04-30 09:26:32 -0600

    Add function to start unique job

---
+++ .shDotFileSupport/functions/screenStartJob.sh
@@ -0,0 +1,26 @@
+# Start a job with a unique ID in a screen and
+# disallow jobs of the same ID until it finishes;
+# Good for hardcoded commands, e.g. in iOS Shortcuts
+
+function screenStartJob {
+	ID="${1/\./}" # Disallow . character
+	ID=ID | xargs # Trim whitespace
+	CT=$(
+		screen -ls | \
+			grep "^\t" | \
+			cut -f 2 | \
+			cut -f 2 -d . | \
+			grep $ID -F | \
+			grep "" -c
+	)
+	CT=$(($CT + 0))
+	if [ $CT -ge 1 ]
+	then
+		echo "There is already a screen called $1."
+	else
+		CMD="screen -S $ID -dm $2"
+		eval $CMD
+	fi
+}
+
+echo Sourced!

cf2e4a3e7532292fece934fb77e7f1c626d2fd7f
Jon Dayley <jonathandayley@gmail.com>
2021-06-29 13:45:42 -0600

    Separate right prompt for VCS into options and assignment

--- .shDotFileSupport/prompt.sh
+++ .shDotFileSupport/prompt.sh
@@ -1,18 +1,20 @@
 # Set PS1
 PS1='%F{1}%n%f%F{8}@%f%F{7}%B%m%b%f %F{69}%1~%f %# '
 
-# Set right prompt with version control information
+# Set right prompt VCS options
 setopt PROMPT_SUBST
 autoload -Uz vcs_info
 zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:*' formats \
-	'%B%F{6}%r%f%%b%F{8}→%f%F{214}%b%f%F{green}%c%f%F{red}%u%f'
-zstyle ':vcs_info:*' actionformats \
-	'%B%a%%b %B%F{6}%r%f%%b%F{8}→%f%F{214}%b%f%F{green}%c%f%F{red}%u%f'
 zstyle ':vcs_info:*' stagedstr '*'
 zstyle ':vcs_info:*' unstagedstr '*'
 precmd () { vcs_info }
 RPROMPT='${vcs_info_msg_0_}'
+
+# Set right prompt with VCS info
+zstyle ':vcs_info:*' formats \
+	'%B%F{6}%r%f%%b%F{8}→%f%F{214}%b%f%F{green}%c%f%F{red}%u%f'
+zstyle ':vcs_info:*' actionformats \
+	'%B%a%%b %B%F{6}%r%f%%b%F{8}→%f%F{214}%b%f%F{green}%c%f%F{red}%u%f'
 
 # Set right prompt with Git information manually
 #function buildRightPrompt() {

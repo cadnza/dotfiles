6bc70eb366e99d64535f23edd346999ce06070dd
Jon Dayley <jonathandayley@gmail.com>
2021-06-29 15:59:40 -0600

    Add timeout to only show git diff indicator on repos that don't take a long time to query

--- .shDotFileSupport/prompt.sh
+++ .shDotFileSupport/prompt.sh
@@ -5,10 +5,8 @@ PS1='%F{1}%n%f%F{8}@%f%F{7}%B%m%b%f %F{69}%1~%f %# '
 setopt PROMPT_SUBST
 autoload -Uz vcs_info
 zstyle ':vcs_info:*' enable git
-zstyle ':vcs_info:*' check-for-changes false # Set to true to show diff status markers
 zstyle ':vcs_info:*' stagedstr '*'
 zstyle ':vcs_info:*' unstagedstr '*'
-precmd () { vcs_info }
 
 # Set right prompt with VCS info
 buildRightPrompt() {
@@ -22,6 +20,27 @@ buildRightPrompt() {
 zstyle ':vcs_info:*' formats $(buildRightPrompt)
 zstyle ':vcs_info:*' actionformats $(buildRightPrompt 1)
 RPROMPT='${vcs_info_msg_0_}'
+
+# Get function to measure timeout git status (to only show git diffs when economical)
+timeoutGitStatusDiff() {
+	inner() {
+		delay=0.13
+		timeout $delay zsh -c "{
+			git status > /dev/null;
+			echo true;
+		}"
+		echo false;
+	}
+	enable=$(echo $(inner) | cut -d " " -f 1)
+	echo $enable
+}
+
+# Configure command to run before every prompt
+precmd () {
+	useDiffIndicator=$(timeoutGitStatusDiff)
+	zstyle ':vcs_info:*' check-for-changes $useDiffIndicator
+	vcs_info
+}
 
 # Set right prompt with Git information manually
 #buildRightPrompt() {

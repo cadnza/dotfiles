61367ed2fdf449d7baaac93f9412ec79e17e9072
Jon Dayley <jonathandayley@gmail.com>
2021-06-29 15:01:29 -0600

    Add function to get VCS right prompt string

--- .shDotFileSupport/prompt.sh
+++ .shDotFileSupport/prompt.sh
@@ -4,16 +4,23 @@ PS1='%F{1}%n%f%F{8}@%f%F{7}%B%m%b%f %F{69}%1~%f %# '
 # Set right prompt VCS options
 setopt PROMPT_SUBST
 autoload -Uz vcs_info
+zstyle ':vcs_info:*' enable git
 zstyle ':vcs_info:*' check-for-changes false # Set to true to show diff status markers
 zstyle ':vcs_info:*' stagedstr '*'
 zstyle ':vcs_info:*' unstagedstr '*'
 precmd () { vcs_info }
 
 # Set right prompt with VCS info
-zstyle ':vcs_info:*' formats \
-	'%B%F{6}%r%f%%b%F{8}→%f%F{214}%b%f%F{green}%c%f%F{red}%u%f'
-zstyle ':vcs_info:*' actionformats \
-	'%B%a%%b %B%F{6}%r%f%%b%F{8}→%f%F{214}%b%f%F{green}%c%f%F{red}%u%f'
+buildRightPrompt() {
+	final=%B%F{6}%r%f%%b%F{8}→%f%F{214}%b%f%F{green}%c%f%F{red}%u%f
+	if [ -n "$1" ];
+	then
+		final=$(echo %B%a%%b "$final")
+	fi
+	echo $final
+}
+zstyle ':vcs_info:*' formats $(buildRightPrompt)
+zstyle ':vcs_info:*' actionformats $(buildRightPrompt 1)
 RPROMPT='${vcs_info_msg_0_}'
 
 # Set right prompt with Git information manually

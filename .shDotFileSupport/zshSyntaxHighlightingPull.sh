7dcf73a3fdec47c02d3683e05e52bd302f81268e
Jon Dayley <jonathandayley@gmail.com>
2021-07-12 10:10:11 -0600

    Refactor highlight function to persist syntax highlighting between sessions

--- .shDotFileSupport/zshSyntaxHighlightingPull.sh
+++ .shDotFileSupport/zshSyntaxHighlightingPull.sh
@@ -1,20 +1,45 @@
-echo "NOTE: zsh-syntax-highlighting isn't available as a package for $OS yet,"
-echo -e "but you can source the script directly from the repo by running \e[32mhighlight\e[0m."
+# Record reference to temporary directory containing repo
+fileZshSynHighRef=~/.zshSyntaxHighlightingDirectoryReference
 
-highlight() {
-	if [[ $alreadyHighlighted = 1 ]]
-	then
-		echo "You've already enabled zsh-syntax-highlighting."
-		return
-	fi
-	tempDir=$(mktemp -d)
-	echo "Cloning repo..."
-	zshSyntaxHighlightingURL=https://github.com/zsh-users/zsh-syntax-highlighting.git
-	git clone $zshSyntaxHighlightingURL $tempDir
-	echo "Enabling highlighting..."
-	source $tempDir/zsh-syntax-highlighting.zsh
-	echo "Removing repo..."
-	rm -rf $tempDir
-	echo "Done!"
-	export alreadyHighlighted=1
+# Get function to create a temporary directory and record its location
+getAndRecordTempDir() {
+	hDir=$(mktemp -d)
+	echo $hDir > "$fileZshSynHighRef"
+	echo $hDir
+}
+
+# Create a new temporary directory with reference if the ref or dir doesn't exist
+export HIGHLIGHTING_DIRECTORY=$(cat $fileZshSynHighRef 2> /dev/null || getAndRecordTempDir)
+ls $HIGHLIGHTING_DIRECTORY &> /dev/null || export HIGHLIGHTING_DIRECTORY=$(getAndRecordTempDir)
+
+# Identify target script
+target=$HIGHLIGHTING_DIRECTORY/zsh-syntax-highlighting.zsh
+
+# Source target script or make highlight function available on failure
+source $target 2> /dev/null || {
+	# Echo messages
+	echo "NOTE: zsh-syntax-highlighting isn't available as a package for $OS"
+	echo -e "yet. You can source it directly from the repo by running \e[32mhighlight\e[0m, which"
+	echo "will enable syntax highlighting until you restart your computer or clear"
+	echo "your temporary files."
+	# Define highlight function
+	highlight() {
+		# Return if highlighting is already enabled
+		if [[ -n "$ZSH_HIGHLIGHT_VERSION" ]]
+		then
+			echo "You already have zsh-syntax-highlighting enabled."
+			return
+		fi
+		# Source target script or clone and source on failure
+		source $target 2> /dev/null || {
+			echo "Cloning repo..."
+			zshSyntaxHighlightingURL=https://github.com/zsh-users/zsh-syntax-highlighting.git
+			git clone $zshSyntaxHighlightingURL $HIGHLIGHTING_DIRECTORY
+			echo "Enabling highlighting..."
+			source $target
+			echo "Finishing..."
+			rm -rf "$HIGHLIGHTING_DIRECTORY/.git"
+			echo "Done!"
+		}
+	}
 }

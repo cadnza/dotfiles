4a0f91ded3daf8f9dd169c67553d0cc0c56198bf
Jon Dayley <jonathandayley@gmail.com>
2021-08-06 11:17:02 -0600

    Remove empty space

--- .shDotFileSupport/functions.sh
+++ .shDotFileSupport/functions.sh
@@ -1,7 +1,6 @@
 # Start a job with a unique ID in a screen and
 # disallow jobs of the same ID until it finishes;
 # Good for hardcoded commands, e.g. in iOS Shortcuts
-
 function screenStartJob {
 	ID="${1/\./}" # Disallow . character
 	ID=ID | xargs # Trim whitespace
@@ -24,7 +23,6 @@ function screenStartJob {
 }
 
 # Go to the top level of a Git repo
-
 gd() {
 	root=$(git rev-parse --show-toplevel) 2> /dev/null || {
 		echo -e "\e[32mgd\e[0m only works inside a Git repo."

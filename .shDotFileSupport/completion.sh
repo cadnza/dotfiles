071266ab736d9b543b9aa3af7eb3023c5fd52137
Jon Dayley <jonathandayley@gmail.com>
2021-08-18 15:35:56 -0600

    Bold selected completion menu item

--- .shDotFileSupport/completion.sh
+++ .shDotFileSupport/completion.sh
@@ -9,4 +9,4 @@ zstyle ':completion:*' menu select
 bindkey '\e[Z' reverse-menu-complete # Shift tab
 
 # Match completion colors to LS colors
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS} $(echo "ma=38;5;0;48;5;$colorMachine")
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS} $(echo "ma=38;5;0;48;5;$colorMachine;1")

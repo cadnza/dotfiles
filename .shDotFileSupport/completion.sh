e28532cd3be2793c6ea9b566a32f78b1e9546e82
Jon Dayley <jonathandayley@gmail.com>
2021-08-18 08:01:26 -0600

    Add shift tab for reverse menu completion

--- .shDotFileSupport/completion.sh
+++ .shDotFileSupport/completion.sh
@@ -6,6 +6,7 @@ compinit
 
 # Enable completion menu selector
 zstyle ':completion:*' menu select
+bindkey '\e[Z' reverse-menu-complete # Shift tab
 
 # Match completion colors to LS colors
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}

35ac24a9d900ef7401e9909b791a79a1bb9dfe9e
Jon Dayley <jonathandayley@gmail.com>
2021-09-09 07:23:32 -0600

    Add wrapper for Prettier for nano

---
+++ .shDotFileSupport/nano/formatters/prettier.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+# Make file inferrable to Prettier
+ext=".$1"
+fOriginal=$2
+fRenamed="$fOriginal.$ext"
+mv $fOriginal $fRenamed
+
+# Format
+# Note that .prettierrc doesn't need to be explicitly specified, but it does make
+# the editor error after the fact if there's no .prettierrc, which is what we want.
+prettier --config $HOME/.prettierrc --write $fRenamed
+
+# Replace file
+mv $fRenamed $fOriginal
+
+# Exit
+exit 0

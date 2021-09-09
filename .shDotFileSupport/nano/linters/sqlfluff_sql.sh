cdd2938e5f20e93eef13bfae78c228245bb7c209
Jon Dayley <jonathandayley@gmail.com>
2021-09-09 16:34:47 -0600

    Touch up regex

--- .shDotFileSupport/nano/linters/sqlfluff_sql.sh
+++ .shDotFileSupport/nano/linters/sqlfluff_sql.sh
@@ -9,8 +9,8 @@ do
 	# Set delimiter
 	delim="|"
 	# Get line and column numbers
-	rw=$(echo $msg | cut -d $delim -f 1 | grep -o "[0-9]*")
-	cn=$(echo $msg | cut -d $delim -f 2 | grep -o "[0-9]*")
+	rw=$(echo $msg | cut -d $delim -f 1 | grep -Eo "[0-9]+")
+	cn=$(echo $msg | cut -d $delim -f 2 | grep -Eo "[0-9]+")
 	# Get linting note
 	note=$(echo $msg | cut -d $delim -f 4 | sed 's/^ *//g' | sed 's/ *$//g')
 	# Add to final

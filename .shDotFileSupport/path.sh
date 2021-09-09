c116ba835e94cba9ab8e5c1bd3043508008936a4
Jon Dayley <jonathandayley@gmail.com>
2021-09-09 13:30:02 -0600

    Add hidden local to path

--- .shDotFileSupport/path.sh
+++ .shDotFileSupport/path.sh
@@ -6,6 +6,9 @@ export PATH=/usr/local/sbin:$PATH
 # Add user binaries to path
 export PATH=$HOME/bin:$PATH
 
+# Add hidden local to path (needed for some executables)
+export PATH=$HOME/.local/bin:$PATH
+
 # Add nano linter and formatter directories to path
 export PATH=$HOME/.shDotFileSupport/nano/linters:$PATH
 export PATH=$HOME/.shDotFileSupport/nano/formatters:$PATH

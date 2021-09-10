fc6035f8c6f7c4271a8ea4953e0d166649d9dcc6
Jon Dayley <jonathandayley@gmail.com>
2021-09-10 07:20:41 -0600

    Remove screenStartJob (never used)

--- .shDotFileSupport/functions.sh
+++ .shDotFileSupport/functions.sh
@@ -1,29 +1,5 @@
 #!/usr/bin/env zsh
 
-# Start a job with a unique ID in a screen and
-# disallow jobs of the same ID until it finishes;
-# Good for hardcoded commands, e.g. in iOS Shortcuts
-function screenStartJob {
-	ID="${1/\./}" # Disallow . character
-	ID=ID | xargs # Trim whitespace
-	CT=$(
-		screen -ls | \
-			grep "^\t" | \
-			cut -f 2 | \
-			cut -f 2 -d . | \
-			grep $ID -F | \
-			grep "" -c
-	)
-	CT=$(($CT + 0))
-	if [ $CT -ge 1 ]
-	then
-		echo "There is already a screen called $1."
-	else
-		CMD="screen -S $ID -dm $2"
-		eval $CMD
-	fi
-}
-
 # Go to the top level of a Git repo
 gd() {
 	root=$(git rev-parse --show-toplevel) 2> /dev/null || {

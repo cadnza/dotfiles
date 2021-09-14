b6130a27390d7ced18c687ac09f1134ca91af73c
Jon Dayley <jonathandayley@gmail.com>
2021-09-14 17:33:04 -0600

    Advance mxc function

--- micro/plug/mxc/mxc.lua
+++ micro/plug/mxc/mxc.lua
@@ -4,13 +4,25 @@ local micro = import("micro")
 local buffer = import("micro/buffer")
 local shell = import("micro/shell")
 local config = import("micro/config")
+local util = import("micro/util")
 
 function init()
 	config.MakeCommand("mxc", _mxc, config.OptionComplete)
+	config.TryBindKey("F5","lua:mxc._mxc",false)
 end
 
 function _mxc()
-	local fPath = micro.CurPane().Buf.AbsPath
-	local str, err = shell.RunInteractiveShell(fPath, true, false)
-	micro.InfoBar():Message(err) --TEMP
+	-- Get current pane
+	local bp = micro.CurPane()
+	-- Save buffer
+	bp:Save()
+	micro.InfoBar():ClearInfo()
+	-- Get path of current file in buffer
+	local fPath = bp.Buf.AbsPath
+	-- Run file in perl
+	local str, err = shell.RunInteractiveShell("perl "..fPath, true, false)
+	-- Show any error
+	if err ~= nil then
+		micro.InfoBar():Error(err)
+	end
 end

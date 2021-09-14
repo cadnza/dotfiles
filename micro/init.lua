38492e864a33b51bc5d638ab85e4f501eae81627
Jon Dayley <jonathandayley@gmail.com>
2021-09-14 07:34:35 -0600

    Add linters

---
+++ micro/init.lua
@@ -0,0 +1,38 @@
+function init()
+	linter.makeLinter(
+		"lintr",
+		"r",
+		"lintr_r.R",
+		{"%f"},
+		"%f:%l:%c: %m",
+		{},
+		false,
+		false,
+		0,
+		0
+	)
+	linter.makeLinter(
+		"pylint",
+		"python",
+		"pylint_python.sh",
+		{"%f"},
+		"%f:%l:%c: %m",
+		{},
+		false,
+		false,
+		0,
+		0
+	)
+    linter.makeLinter(
+		"sqlfluff",
+		"sql",
+		"sqlfluff_sql.sh",
+		{"%f"},
+		"%f:%l:%c: %m",
+		{},
+		false,
+		false,
+		0,
+		0
+	)
+end

391d45fbf2b454bf0270fadaa716967572ff1ce8
Jon Dayley <jonathandayley@gmail.com>
2021-09-15 07:51:04 -0600

    Finish plugin script

--- micro/plug/mxc/mxc.lua
+++ micro/plug/mxc/mxc.lua
@@ -1,16 +1,18 @@
 VERSION = "1.0.0"
 
 local micro = import("micro")
-local buffer = import("micro/buffer")
 local shell = import("micro/shell")
 local config = import("micro/config")
-local util = import("micro/util")
 
 function init()
+	-- Create commands
 	config.MakeCommand("mxc", mxc, config.OptionComplete)
+	config.MakeCommand("mxcx", mxcx, config.OptionComplete)
+	-- Map bindings
 	config.TryBindKey("F5","lua:mxc.mxc",false)
-	config.MakeCommand("mxc", mxcx, config.OptionComplete)
 	config.TryBindKey("F6","lua:mxc.mxcx",false)
+	-- Set options
+	config.RegisterCommonOption("mxc", "saveOnRun", true)
 end
 
 function mxc()
@@ -24,9 +26,11 @@ end
 function driver(lookForMxcFile)
 	-- Get current pane
 	local bp = micro.CurPane()
-	-- Save buffer -- Make this optional --TEMP
-	bp:Save()
-	micro.InfoBar():ClearInfo()
+	-- Save buffer if option set
+	local doSave = config.GetGlobalOption("mxc.saveOnRun")
+	if doSave then
+		bp:Save()
+	end
 	-- Get path of current file in buffer
 	local fPath = bp.Buf.AbsPath
 	-- Run main script

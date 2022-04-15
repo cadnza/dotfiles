175cee56f1422997bfc0e8e951e28fe63a5602ed
Jon Dayley <jonathandayley@gmail.com>
2022-04-15 10:39:52 -0700

    Enable completion for op if installed

--- .shDotFileSupport/completion.sh
+++ .shDotFileSupport/completion.sh
@@ -10,3 +10,7 @@ bindkey '\e[Z' reverse-menu-complete # Shift tab
 
 # Match completion colors to LS colors
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS} $(echo "ma=38;5;0;48;5;$colorMachine;1")
+
+# Enable op completion if op is installed
+eval "$(op completion zsh 2> /dev/null)" &> /dev/null
+compdef _op op &> /dev/null

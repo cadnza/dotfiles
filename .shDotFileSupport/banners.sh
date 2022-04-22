7359c0f9c023b9947065fc2433c995234643a3f1
Jon Dayley <jonathandayley@gmail.com>
2022-04-22 15:17:58 -0700

    Add logic to count unattached screens for banner

--- .shDotFileSupport/banners.sh
+++ .shDotFileSupport/banners.sh
@@ -15,7 +15,9 @@ tmux ls &> /dev/null && {
 dS=[[:digit:]]
 [[ $(screen -ls | head -n 1 | grep -c "^No Sockets") = 1 ]] || {
 	ct=$(screen -ls | grep -Eo "^$dS$dS* Socket" | grep -Eo "^$dS$dS*")
+	ctAttached=$(screen -ls | grep -cEo "\(Attached\)$")
+	ct=$(($ct-$ctAttached))
 	screensWord="\033[1mscreen\033[22m"
-	[[ $ct = 1 ]]|| screensWord=$screensWord"s"
-	echo -e "\033[48;5;4m$ct $screensWord open\033[0m"
+	[[ $ct = 1 ]] || screensWord=$screensWord"s"
+	echo -e "\033[48;5;4m$ct unattached $screensWord open\033[0m"
 }

ad38fc33f6c69c6c461e73d9965a81ece2af7a53
Jon Dayley <jonathandayley@gmail.com>
2022-08-14 09:54:38 -0700

    Add logic to control for possible race condition

--- .shDotFileSupport/scripts/exit.sh
+++ .shDotFileSupport/scripts/exit.sh
@@ -7,6 +7,9 @@ tell application "Terminal"
 			set end of busyWndws to wndw
 		end if
 	end repeat
+	if (count of busyWndws) is 0 then
+		quit
+	end if
 	if (count of busyWndws) is 1 then
 		set wndw to item 1 of busyWndws
 		if processes of wndw is {"login", "-zsh", "screen"} then

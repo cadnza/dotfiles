1546e01899cabee3ad48d90868a68913012dec66
Jon Dayley <jonathandayley@gmail.com>
2023-02-19 14:20:16 -0800

    Rename

--- .support/completion.sh
+++ .support/completion.sh
@@ -12,7 +12,7 @@ bindkey '\e[Z' reverse-menu-complete # Shift tab
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS} $(echo "ma=38;5;0;48;5;$colorMachine;1")
 
 # Source completions
-find $HOME/.support/completions -type f | while read -r f
+find $HOME/.support/completion_scripts -type f | while read -r f
 do
 	source $f
 done

d7ca1f68b4593d148c882d4df447b6e9c362fc10
Jon Dayley <jonathandayley@gmail.com>
2023-08-23 11:17:40 -0600

    Update banner

--- ps1/bah.ps1
+++ ps1/bah.ps1
@@ -6,5 +6,5 @@ $cMachine = "$([char]27)[38;2;255;255;255m"
 $cDirectory = "$([char]27)[3m$([char]27)[38;2;1;128;126m"
 
 # Show logo
-$bahLogo = "$([char]27)[1m$([char]27)[48;2;1;128;126m$([char]27)[38;2;255;255;255m Booz | Allen | Hamilton $([char]27)[0m"
-Write-Host $bahLogo
+$bahPre = "$([char]27)[1m$([char]27)[48;2;1;128;126m$([char]27)[38;2;255;255;255m"
+Write-Host "$bahPre Booz | Allen | Hamilton $charReset"

4bdb535e2e6ef4235eb7d292760115a341fb19a5
Jon Dayley <jonathandayley@gmail.com>
2023-08-23 09:19:58 -0600

    Start poshgit logic

--- profile.ps1
+++ profile.ps1
@@ -4,24 +4,40 @@ if (Test-Path($ChocolateyProfile)) {
 	Import-Module "$ChocolateyProfile"
 }
 
+# Import posh-git for git indicators
+$poshgitPsd = "$HOME\Repos\posh-git\src\posh-git.psd1"
+if ( Test-Path -Path $poshGitPsd) {
+	Import-Module $poshgitPsd
+	$hasPoshgit = $true
+}
+else {
+	Write-Host "$([char]27)[41;30mposh-git not installed; see https://github.com/dahlbyk/posh-git$([char]27)[0m"
+	$hasPoshgit = $false
+}
+
 # Define Microsoft colors (https://usbrandcolors.com/microsoft-colors/)
-$msOrange = "242;80;34"
-$msGreen = "127;186;0"
-$msBlue = "0;164;239"
-$msYellow = "255;185;0"
-$msGrey = "115;115;115"
+$cMsOrange = "242;80;34"
+$cMsGreen = "127;186;0"
+$cMsBlue = "0;164;239"
+$cMsYellow = "255;185;0"
+$cMsGrey = "115;115;115"
 
 # Define characters
 $charSep = " | "
 $charReset = "$([char]27)[0m"
 
 # Set colors
-$colorUser = "$([char]27)[38;2;$msOrange" + "m"
-$colorSep = "$([char]27)[38;2;$msGrey" + "m"
-$colorMachine = "$([char]27)[38;2;$msYellow" + "m"
-$colorDirectory = "$([char]27)[38;2;$msBlue" + "m"
+$cUser = "$([char]27)[38;2;$cMsOrange" + "m"
+$cSep = "$([char]27)[38;2;$cMsGrey" + "m"
+$cMachine = "$([char]27)[38;2;$cMsYellow" + "m"
+$cDirectory = "$([char]27)[38;2;$cMsBlue" + "m"
 
 # Set prompt
 function prompt {
-	"$colorUser$env:USERNAME$charReset$colorSep$charSep$charReset$colorMachine$env:COMPUTERNAME$charReset $colorDirectory$( Split-Path $PWD -Leaf )$charReset "
+	$final = "$cUser$env:USERNAME$charReset$cSep$charSep$charReset$cMachine$env:COMPUTERNAME$charReset $cDirectory$( Split-Path $PWD -Leaf )$charReset "
+
+	if ($hasPoshgit) {
+	}
+
+	return $final
 }

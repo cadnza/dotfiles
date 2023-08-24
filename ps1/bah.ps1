486fa0307f43bb08bd76ec3540de206367d44eee
Jon Dayley <jonathandayley@gmail.com>
2023-08-24 10:24:36 -0600

    Redo bolding

--- ps1/bah.ps1
+++ ps1/bah.ps1
@@ -7,12 +7,12 @@ $cMachine = "$([char]27)[38;2;255;255;255m"
 $cDirectory = "$([char]27)[3m$([char]27)[38;2;$cBah" + "m"
 
 # Define logo
-$bahPre = "$([char]27)[1m$([char]27)[48;2;$cBah" + "m$([char]27)[38;2;255;255;255m"
+$bahPre = "$([char]27)[48;2;$cBah" + "m$([char]27)[38;2;255;255;255m"
 $bahSpace = "                                      "
 $bahLogo = $bahPre
 $bahLogo += $bahSpace
 $bahLogo += "`n"
-$bahLogo += "       Booz | Allen | Hamilton        "
+$bahLogo += "       $([char]27)[1mBooz$([char]27)[22m | $([char]27)[1mAllen$([char]27)[22m | $([char]27)[1mHamilton$([char]27)[22m        "
 $bahLogo += "`n"
 $bahLogo += $bahSpace
 $bahLogo += "`n"

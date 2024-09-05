b5d7d10bd9f72970a43734c966f540d2e18313a7
Jon Dayley <jonathandayley@gmail.com>
2024-09-05 09:43:53 -0600

    Add terraform completion

---
+++ .support/completion_scripts/terraform.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env false
+
+# Enable terraform completion if terraform is installed
+autoload -U +X bashcompinit && bashcompinit
+complete -o nospace -C /opt/homebrew/bin/terraform terraform &> /dev/null

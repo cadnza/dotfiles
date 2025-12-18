281ac903ddd3f0104a88898804c5b9237d8aeb31
Jon Dayley <jonathandayley@gmail.com>
2025-12-18 14:05:46 -0700

    Reorder path

--- .support/path.sh
+++ .support/path.sh
@@ -1,9 +1,6 @@
 # Add sbin to path
 export PATH=/usr/local/sbin:$PATH
 
-# Add hidden local to path (this is where user executables go)
-export PATH=$HOME/.local/bin:$PATH
-
 # Add user binaries to path (for canonicity's sake; don't deliberately put any user executables here)
 export PATH=$HOME/bin:$PATH
 
@@ -20,6 +17,9 @@ export PATH=$PATH:$HOME/.pub-cache/bin
 # You can list registered URI scheme bindings with `lsregister -dump URLSchemeBinding`.
 # Optionally throw on a `| sort` to sort the output.
 export PATH=$PATH:/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/
+
+# Add hidden local to path (this is where user executables go)
+export PATH=$HOME/.local/bin:$PATH
 
 # Add path to cargo binaries
 export PATH=$PATH:$HOME/.cargo/bin

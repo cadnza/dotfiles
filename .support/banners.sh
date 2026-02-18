f7c3f706038590096775fbc78ef8c7db8388be59
Jon Dayley <jonathandayley@gmail.com>
2026-02-18 12:32:41 -0700

    Update thing

--- .support/banners.sh
+++ .support/banners.sh
@@ -15,8 +15,8 @@ dS='[[:digit:]]'
     ct=$(screen -ls | grep -Eo "^$dS$dS* Socket" | grep -Eo "^$dS$dS*")
     ct_attached=$(screen -ls | grep -cEo "\(Attached\)$")
     ct=$((ct - ct_attached))
-    screens_word="\033[1mscreen\033[22m"
+    screens_word=screen
     [[ $ct = 1 ]] || screens_word=$screens_word"s"
     [[ $ct -ge 1 ]] &&
-        printf '\033[37;44m%s detached \033[1mscreen\033[22m %s\033[0m\n' $ct "$sessions_word" >&2
+        printf '\033[37;44m%s detached \033[1m%s\033[22m\033[0m\n' $ct "$screens_word" >&2
 }
